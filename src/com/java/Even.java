f7b3da3238cac8659ae0554045cd0d8961d88982
Jiten <jiten.ahuja47@gmail.com>
2022-12-16 16:12:39 +0530

    first commit

---
+++ src/com/java/Even.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class Even {
+	public static void main(String[] args) {
+		
+		int i = 2;
+		if(i%2 == 0)
+		{
+			System.out.println("its even");
+		}
+		else
+		{
+			System.out.println("Its not even");
+		}
+	}
+
+}
